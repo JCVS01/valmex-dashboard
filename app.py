588cc286176ea9fd93d5a6845f60cf366a9fcf20
JCVS01 <jvilla@valmex.com.mx>
2026-02-19 08:30:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,17 @@ app = Flask(__name__)
 def index():
     return send_file("valmex_dashboard.html")
 
-@app.route("/presentacion.pdf")
+@app.route("/PC.pdf")
 def pdf():
-    return send_file("PRESENTACIO_N_COMERCIAL.pdf")
+    return send_file("PC.pdf")
+
+@app.route("/VALMEX.png")
+def logo1():
+    return send_file("VALMEX.png")
+
+@app.route("/VALMEX2.png")
+def logo2():
+    return send_file("VALMEX2.png")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
